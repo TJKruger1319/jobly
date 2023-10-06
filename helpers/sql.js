0de1c5b5fde7c31788f1566c24c748c2d50cb829
TJ <TJKruger1319@gmail.com>
2023-10-06 19:24:19 -0400

    Adds documentation and tests for sqlForPartialUpdate

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,6 +1,20 @@
 const { BadRequestError } = require("../expressError");
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
+/**
+ * Helper function that transforms data into SQL readable format
+ * 
+ * This is to be used with the SET clause of an SQL UPDATE statement.
+ * 
+ * 
+ * @param dataToUpdate {object} {entry1: val1, entry2: val2, ...}
+ * @param jsToSql {object} maps js-style data fields to database column names
+ * 
+ * @returns {Object} {sqlSetCols, dataToUpdate}
+ * 
+ * @example {firstName: 'Shawn', age: 19} =>
+ *  { setCols: '"first_name"=$1, "age"=$2',
+ *  values: ['Shawn', 19]}
+ */
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
